d3236cc31678dce832e5a6cf51e83efd24563f1f
petr524 <128919859+petr524@users.noreply.github.com>
2024-12-02 08:01:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ function broadcast(data, excludeWs) {
 }
 
 // Spuštění serveru
-const PORT = 3306;
+const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`Server běží na http://0.0.0.0:${PORT}`);
 });
